185fccc8d2b1d6d7f499a9b44d5022ef84de3e75
Eric Blossom <eb@comsec.com>
2010-07-26 23:17:58 -0700

    Device simulator that just echos whatever is sent to it

--- vrt-ctrl/device/sim/dev-sim-main.cc
+++ vrt-ctrl/device/sim/dev-sim-main.cc
@@ -20,6 +20,7 @@
 #include <string>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <vrtc/device/handle_datagram.h>
 
 using boost::asio::ip::udp;
 
@@ -30,25 +31,51 @@ std::string make_daytime_string()
   return ctime(&now);
 }
 
-enum { dev_sim_ctrl_port = 4795 };
 
-// enum { max_buffer = 4096; }
+enum { DEV_SIM_CTRL_PORT = 4795 };
+enum { MAX_BUFFER = 1500 };
+
+struct server_state {
+  server_state(boost::asio::io_service &io) :
+    socket(io, udp::v4())
+  {
+  }
+  udp::socket	socket;
+  udp::endpoint remote_endpoint;
+
+};
+
+extern "C" {
+  void dev_sim_send_dgram(void *handle, const void *buf, size_t len)
+  {
+    server_state *ss = (server_state *) handle;
+    boost::system::error_code ignored_error;
+    ss->socket.send_to(boost::asio::buffer(buf, len),
+		       ss->remote_endpoint, 0, ignored_error);
+  }
+}
 
 int main()
 {
   try
   {
     boost::asio::io_service io_service;
+    server_state st(io_service);
+    boost::system::error_code ec;
 
-    udp::socket socket(io_service, udp::endpoint(udp::v4(), dev_sim_ctrl_port));
+    st.socket.bind(udp::endpoint(udp::v4(), DEV_SIM_CTRL_PORT), ec);
+    if (ec){
+      std::cerr << ec.message() << ": Failed to bind udp socket on port "
+		<< DEV_SIM_CTRL_PORT << std::endl;
+      return 0;
+    }
 
     for (;;)
     {
-      boost::array<char, 1> recv_buf;
-      udp::endpoint remote_endpoint;
+      boost::array<char, MAX_BUFFER> recv_buf;
       boost::system::error_code error;
-      socket.receive_from(boost::asio::buffer(recv_buf),
-          remote_endpoint, 0, error);
+      st.socket.receive_from(boost::asio::buffer(recv_buf),
+			     st.remote_endpoint, 0, error);
 
       if (error && error != boost::asio::error::message_size)
         throw boost::system::system_error(error);
@@ -56,8 +83,8 @@ int main()
       std::string message = make_daytime_string();
 
       boost::system::error_code ignored_error;
-      socket.send_to(boost::asio::buffer(message),
-          remote_endpoint, 0, ignored_error);
+      st.socket.send_to(boost::asio::buffer(message),
+			st.remote_endpoint, 0, ignored_error);
     }
   }
   catch (std::exception& e)
