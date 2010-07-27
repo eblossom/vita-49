63f9f56dfb77381fd2f29008a040788f8352dc6d
Eric Blossom <eb@comsec.com>
2010-07-26 17:23:11 -0700

    vrtc-dev-sim builds and runs

---
+++ vrt-ctrl/device/sim/dev-sim-main.cc
@@ -0,0 +1,69 @@
+//
+// dev-sim-main.cc -- blocking UDP server that serves as top level of device-simulator
+//
+// Copyright 2010 Free Software Foundation, Inc.
+//
+//
+// Derived from:
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2010 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+enum { dev_sim_ctrl_port = 4795 };
+
+// enum { max_buffer = 4096; }
+
+int main()
+{
+  try
+  {
+    boost::asio::io_service io_service;
+
+    udp::socket socket(io_service, udp::endpoint(udp::v4(), dev_sim_ctrl_port));
+
+    for (;;)
+    {
+      boost::array<char, 1> recv_buf;
+      udp::endpoint remote_endpoint;
+      boost::system::error_code error;
+      socket.receive_from(boost::asio::buffer(recv_buf),
+          remote_endpoint, 0, error);
+
+      if (error && error != boost::asio::error::message_size)
+        throw boost::system::system_error(error);
+
+      std::string message = make_daytime_string();
+
+      boost::system::error_code ignored_error;
+      socket.send_to(boost::asio::buffer(message),
+          remote_endpoint, 0, ignored_error);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
