1e2eacc592b810757a70ce07d8574d1e4fe64840
Eric Blossom <eb@comsec.com>
2010-07-27 18:21:37 -0700

    Move send_datagram into its own file.

--- vrt-ctrl/include/vrtc/send_datagram.h
+++ vrt-ctrl/include/vrtc/send_datagram.h
@@ -15,30 +15,22 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef INCLUDED_VRTC_DEVICE_HANDLE_DATAGRAM_H
-#define INCLUDED_VRTC_DEVICE_HANDLE_DATAGRAM_H
+#ifndef INCLUDED_VRTC_SEND_DATAGRAM_H
+#define INCLUDED_VRTC_SEND_DATAGRAM_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <vrtc/send_datagram.h>
+#include <stddef.h>	/* size_t */
 
-  /*
-   * \brief Entry point to device control protocol handler.
-   *
-   * \param[in] buf contains the incoming datagram
-   * \param[in] len is the length of \p buf
-   * \param[in] send is used to send one or more reply datagrams
-   * \param[in] handle is the first argument to \p send
-   * \param[in] is the maximum number of bytes which may be sent in one call
-   */
-  void vrtcd_handle_incoming_datagram(void *buf, size_t len,
-				      send_datagram_t send, send_datagram_handle_t handle,
-				      size_t max_send_len);
-			  
+typedef void *send_datagram_handle_t;
+
+/* Pointer to function that will send a datagram reply */
+typedef void (*send_datagram_t)(send_datagram_handle_t handle,
+				const void *buf, size_t len);
+
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* INCLUDED_VRTC_DEVICE_HANDLE_DATAGRAM_H */
+#endif /* INCLUDED_VRTC_SEND_DATAGRAM_H */
