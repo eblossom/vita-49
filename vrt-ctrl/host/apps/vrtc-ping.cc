44234da6c8086bf5d0627b1d4a911eeb884854de
Eric Blossom <eb@comsec.com>
2010-08-02 21:24:52 -0700

    Use connect

--- vrt-ctrl/host/apps/vrtc-ping.cc
+++ vrt-ctrl/host/apps/vrtc-ping.cc
@@ -7,8 +7,11 @@ using boost::asio::ip::udp;
 
 enum { MAX_PAYLOAD = 1500 - 28 };	// MAX UDP payload on typical ethernet
 
-struct udp_state {
-  udp_state(boost::asio::io_service &io) :
+#define STRINGIFY(x) _STRINGIFY(x)
+#define _STRINGIFY(x) #x
+
+struct ctrl_state {
+  ctrl_state(boost::asio::io_service &io) :
     socket(io, udp::v4()) { }
   udp::socket	socket;
   udp::endpoint remote_endpoint;
@@ -17,7 +20,7 @@ struct udp_state {
 extern "C" {
   void dev_sim_send_datagram(void *handle, const void *buf, size_t len)
   {
-    udp_state *us = (udp_state *) handle;
+    ctrl_state *us = (ctrl_state *) handle;
     boost::system::error_code ignored_error;
     us->socket.send_to(boost::asio::buffer(buf, len),
 		       us->remote_endpoint, 0, ignored_error);
@@ -36,21 +39,23 @@ int main(int argc, char* argv[])
     boost::asio::io_service io_service;
 
     udp::resolver resolver(io_service);
-    udp::resolver::query query(udp::v4(), argv[1], "4790" /*VRTC_UDP_CTRL_PORT*/);
+    udp::resolver::query query(udp::v4(), argv[1], STRINGIFY(VRTC_UDP_CTRL_PORT));
     
-    udp_state us(io_service);
+    ctrl_state us(io_service);
     us.remote_endpoint = *resolver.resolve(query);
-    us.socket.open(udp::v4());
+    us.socket.connect(us.remote_endpoint);
+    //std::cout << "remote port = " << us.remote_endpoint.port() << std::endl;
 
     while (1){
       //boost::array<char, 1> send_buf  = { 0 };
       //socket.send_to(boost::asio::buffer(send_buf), remote_endpoint);
-      us.socket.send_to(boost::asio::buffer(std::string("Hello VRTC!\n")), remote_endpoint);
+      //us.socket.send_to(boost::asio::buffer(std::string("Hello VRTC!\n")), us.remote_endpoint);
+      us.socket.send(boost::asio::buffer(std::string("Hello VRTC!\n")));
 
       boost::array<char, 1024> recv_buf;
       udp::endpoint local_endpoint;
       //size_t len = us.socket.receive_from(boost::asio::buffer(recv_buf), local_endpoint);
-      size_t len = us.socket.receive(boost::asio::buffer(recv_buf))
+      size_t len = us.socket.receive(boost::asio::buffer(recv_buf));
       //std::cerr << "vrtc-ping: recv len = " << len << std::endl;
 
       std::cout.write(recv_buf.data(), len);
